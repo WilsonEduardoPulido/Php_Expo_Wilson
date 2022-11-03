75fb94f8f4b2fab3254c4dc6f465d2f4d3a4c966
Wilson Eduardo Pulido <wepulido2@misena.edu.co>
2022-11-03 10:39:15 -0500

    index.php commit #1 3/11/2022

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html lang="es"
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+
+   <h1>Hola mundo </h1>
+
+
+
+<?php
+
+ print 'hola mundo';
+
+?>
+</pre>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
